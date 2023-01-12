6d2ce202d3bcf00de7f97f832cc4de5c68767595
Emmanuel Teye <emmateye42@gmail.com>
2023-01-12 11:26:55 +0000

    added more file to sharedlibrary vars

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,13 @@
  def call(){ 
        node {
-        sh "echo Hello World"
+         stage("build stage") {
+            sh "echo building"
+         }
+         stage("test stage") {
+            sh "echo testing"
+         }
+         stage("deploy stage") {
+            sh "echo deploying"
+         }
        }
   }
